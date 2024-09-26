03ef7ae17a6d8de2785e75098d1bd13788200b3f
Subir Karmakar <69161761+subir-karmakar@users.noreply.github.com>
2024-09-26 21:27:42 +0530

    Delete railwayTicketBackEnd/booking directory

--- railwayTicketBackEnd/booking/src/main/java/com/ortb/booking/repo/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ortb.booking.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ortb.booking.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
--- railwayTicketBackEnd/booking/src/main/java/com/ortb/booking/repo/TrainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ortb.booking.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ortb.booking.model.Train;
-
-public interface TrainRepository extends JpaRepository<Train, Long> {
-
-}
