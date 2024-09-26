a5dfc3e8a5b6e9373486239f008456cda865f6a2
Subir Karmakar <69161761+subir-karmakar@users.noreply.github.com>
2024-09-26 21:28:17 +0530

    Delete railwayTicketBackEnd/trains directory

--- railwayTicketBackEnd/trains/src/main/java/com/ortb/trains/repo/SeatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ortb.trains.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ortb.trains.model.Seat;
-
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
